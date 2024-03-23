2fc001dd237165063e5c4ff56cf645c0e005d4fa
majdhaidar <haidarmajd@hotmail.com>
2024-03-23 19:04:09 +0200

    adding beer repository

---
+++ src/main/java/haidar/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package haidar.springframework.msscbeerservice.repositories;
+
+import haidar.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
